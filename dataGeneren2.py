87b9a487e5e3ca2a5fea57fab72143c9c2844989
SethCardoen <seth.cardoen@gmail.com>
2021-11-12 15:44:16 +0100

    adding_linespace_to_xsample

--- dataGeneren2.py
+++ dataGeneren2.py
@@ -3,25 +3,18 @@ import sympy as sp
 import numpy.random
 
 def xsample(n, a=0, b=1):
-	vector = []
-	v = numpy.random.normal(a,b,n)
-	for i in v:
-		vector.append([i]) 
-	vector = np.asarray(vector)
-	return vector
-
+	return np.linspace(a,b,n)
 
 def ysample(x,f,e=1):
 	vector = []
 	for i in x:
-		for j in i:
-			vector.append([f(j)+(numpy.random.normal(0,1,1)[0])])
+		vector.append([f(i)+(numpy.random.normal(0,1,1)[0])])
 	vector = np.asarray(vector)	
 	return vector	
 def main():
 	x = sp.symbols('s')
 	def f(x): return 2*x
 	print(xsample(8))
-	print(ysample(xsample(50),f))
+	print(ysample(xsample(5000),f))
 
 main()
